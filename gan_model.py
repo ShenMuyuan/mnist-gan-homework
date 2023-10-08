100d1f3346332f77670563ea5ded3aa60903a007
muyuanshen <u202013816@hust.edu.cn>
2023-10-08 13:31:13 +0800

    add model and training code

---
+++ gan_model.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+
+
+class gan_discriminator(nn.Module):
+    def __init__(self):
+        super(gan_discriminator, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(784, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class gan_generator(nn.Module):
+    def __init__(self):
+        super(gan_generator, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(128, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 784),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        return self.layers(x)
